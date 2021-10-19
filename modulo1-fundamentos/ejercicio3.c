d54580d4cf199b70f751a3445d50c8cd649ea287
Jorge Martínez Hurtado <45176388+nothing4free@users.noreply.github.com>
2021-10-19 19:16:57 +0200

    File: added ejercicio3.c

---
+++ modulo1-fundamentos/ejercicio3.c
@@ -0,0 +1,34 @@
+// Calcular el algoritmo que determine la cifra de centenas, decenas y unidades de un número de 3 cifras (del 100 al 999)
+
+#include <stdio.h>
+
+int main() {
+
+    // variables
+    int numero;
+    int centenas;
+    int decenas;
+    int unidades;
+
+    // recoleccion de datos
+    printf("Introduzca un numero entre 100 y 999: \n");
+    scanf("%d", &numero);
+
+    if((numero >= 100) && (numero <= 999)) { // comprobamos que el numero introducido este entre 100 y 999. Si se cumple, calculamos.
+
+        // calculamos las decenas, centenas y unidades
+        centenas = numero / 100;
+        decenas = (numero - (centenas * 100)) / 10;
+        unidades = (numero - (centenas * 100) - (decenas  * 10));
+
+        // mostramos por pantalla el resultado
+        printf("- Centenas de %d: %d \n", numero, centenas);
+        printf("- Decenas de %d: %d \n", numero, decenas);
+        printf("- Unidades de %d: %d \n", numero, unidades);
+
+    } else { // si el numero no esta entre 100 y 999, entonces mostramos un mensaje de error
+        printf("ERROR: El numero tiene que estar entre 100 y 999. \n");
+    }
+
+    return 0;
+}
