0b6068a8dfbb0c132a3651a98aec4cba9776d8df
Jorge Martínez Hurtado <45176388+nothing4free@users.noreply.github.com>
2021-10-19 19:45:46 +0200

    File: added ejercicio7.c

---
+++ modulo1-fundamentos/ejercicio7.c
@@ -0,0 +1,32 @@
+// Elabora un algoritmo que sirva para identificar el tipo de triangulo conociendo sus tres lados
+
+#include <stdio.h>
+
+int main() {
+
+    // variables
+    int lado1;
+    int lado2;
+    int lado3;
+
+    // recoleccion de datos
+    printf("Introduzca el lado 1: \n");
+    scanf("%d", &lado1);
+    printf("Introduzca el lado 2: \n");
+    scanf("%d", &lado2);
+    printf("Introduzca el lado 3: \n");
+    scanf("%d", &lado3);
+
+    // comprobamos que tipo de triangulo es y lo mostramos por pantalla
+    if((lado1 != lado2) && (lado2 != lado3) && (lado1 != lado3)) {
+        printf("El triangulo es escaleno, pues todos sus lados son desiguales. \n");
+    } else {
+        if((lado1 == lado2) && (lado2 == lado3)) {
+            printf("El triangulo es equilatero, pues todos sus lados son iguales. \n");
+        } else {
+            printf("El triangulo es isosceles, pues dos de sus lados son iguales. \n");
+        }
+    }   
+
+    return 0; 
+}
