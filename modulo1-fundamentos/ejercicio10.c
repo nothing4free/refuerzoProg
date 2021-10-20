c40321043fe9bdefdff41353fdefded336edf61e
Jorge Martínez Hurtado <45176388+nothing4free@users.noreply.github.com>
2021-10-20 09:58:26 +0200

    File: added ejercicio9.c and ejercicio10.c

---
+++ modulo1-fundamentos/ejercicio10.c
@@ -0,0 +1,33 @@
+// Elaborar un algoritmo que solicite la edad de 200 personas, y que muestre cuantos son mayores y menores de edad.
+
+#include <stdio.h>
+
+int main() {
+    
+    // variables
+    int edad;
+    int mayores = 0; // en este caso inicializamos las variables a 0 
+    int menores = 0;
+    
+    // bucle que se repetira 200 veces
+    for(int i = 0; i < 200; i++) {
+
+        // recolectamos datos
+        printf("Introduzca la edad: \n");
+        scanf("%d", &edad);
+
+        // determinamos si son mayores o menores
+        if(edad < 18) {
+            menores++;
+        } else {
+            mayores++;
+        }
+    }
+
+    // mostramos por pantalla los resultados
+    printf("La cantidad de personas mayores de edad es: %d \n", mayores);
+    printf("La cantidad de personas menores de edad es: %d \n", menores);
+
+    return 0;
+
+}
---
+++ modulo1-fundamentos/ejercicio9.c
@@ -0,0 +1,31 @@
+// Elabora un algoritmo que solicite 15 números enteros y diferentes a cero, e indique para cada uno si es par
+
+#include <stdio.h>
+
+int main() {
+
+    // variables
+    int numero;
+
+    // pasamos al bucle que comprobara las condiciones del enunciado y determinara si es par o no
+    for(int i = 0; i < 15; i++) { // bucle que se repetira 15 veces
+
+        // recolectamos datos
+        printf("Introduzca un numero distinto de 0: \n");
+        scanf("%d", &numero);
+
+        // comprobamos y determinamos
+        if(numero > 0) {
+            if((numero % 2) == 0) {
+                printf("%d es par. \n", numero);
+            } else {
+                printf("%d es impar. \n", numero);
+            }
+
+        } else {
+            printf("ERROR: el numero tiene que ser entero y diferente de 0.\n");
+            break;
+        }
+    }
+    return 0;
+}
